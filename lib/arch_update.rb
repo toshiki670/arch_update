bf316ed99f52a62076159957075fedc0c50c6aa0
Toshiki <toshiki.dev@protonmail.ch>
2020-05-25 01:57:00 +0900

    Add logger class 'feature/#2'

--- lib/arch_update.rb
+++ lib/arch_update.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
+require 'arch_update/logger'
 require "arch_update/version"
 
 module ArchUpdate
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def logger
+      ArchUpdate::Logger.instance
+    end
+
+    def logger_init(*args)
+      ArchUpdate::Logger.instance.init(*args)
+    end
+  end
 end
---
+++ lib/arch_update/logger.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'delegate'
+require 'singleton'
+require 'logger'
+
+module ArchUpdate
+  class Logger < Delegator
+    include Singleton
+
+    DEFAULT_LEVEL = ::Logger::Severity::INFO
+
+    attr_reader :base
+
+    def initialize
+      init(STDOUT, level: ::Logger::Severity::DEBUG)
+    end
+
+    def init(logdev = default_output, level: ::ArchUpdate::Logger::DEFAULT_LEVEL)
+      @base = ::Logger.new(logdev, level: level)
+      nil
+    end
+
+    def is_a?(*args)
+      @base.is_a?(*args) || super(*args)
+    end
+
+    private
+
+    def default_output
+      "./#{Time.new.strftime('%F_%T')}.log"
+    end
+
+    def __getobj__
+      @base
+    end
+
+    def __setobj__(base)
+      @base = base
+    end
+  end
+end
