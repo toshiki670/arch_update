4f9358362a31197c802a9c6a43c3579abd96f52d
Toshiki <toshiki.dev@protonmail.ch>
2020-05-25 02:00:50 +0900

    Add magic comment 'feature/#2'

--- arch_update.gemspec
+++ arch_update.gemspec
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'lib/arch_update/version'
 
 Gem::Specification.new do |spec|
--- spec/arch_update_spec.rb
+++ spec/arch_update_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe ArchUpdate do
   it "has a version number" do
     expect(ArchUpdate::VERSION).not_to be nil
