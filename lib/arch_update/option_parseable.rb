0ebf48222c87be8061a9397c6dc10aa093ad4c42
Toshiki <toshiki.dev@protonmail.ch>
2020-05-29 07:22:47 +0900

    Fix Error name 'feature/#5'

--- lib/arch_update/option_parseable.rb
+++ lib/arch_update/option_parseable.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ArchUpdate
-  class ExecutableError < StandardError; end
+  class OptionParseableError < NotImplementedError; end
 
   module OptionParseable
     def self.included(base)
