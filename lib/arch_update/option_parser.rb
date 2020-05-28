969124eb1046cdfae4def7b949ef3a61e4a4f655
Toshiki <toshiki.dev@protonmail.ch>
2020-05-29 07:41:16 +0900

    Add parsers 'feature/#5'

--- lib/arch_update/option_parser.rb
+++ lib/arch_update/option_parser.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'arch_update/pacman'
 
 module ArchUpdate
   # The OptionParser's wrapper
   class OptionParser < ::OptionParser
+    PARSERS = [
+      ArchUpdate::Pacman
+    ]
+
+    def check
+      PARSERS.each do |parser|
+        unless parser.include? ArchUpdate::OptionParseable
+          raise ArchUpdate::OptionParseableError
+        end
+      end
+    end
+
     def build
       self.version = ArchUpdate::VERSION
+
+      PARSERS.each do |parser|
+        parser.addon self
+      end 
     end
   end
 end
