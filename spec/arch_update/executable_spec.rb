1cf44072dcbd9e7862f965f2eaef9dc1b03c440a
Toshiki <toshiki.dev@protonmail.ch>
2020-05-31 22:29:24 +0900

    Add executable_spec 'feature/#12'

---
+++ spec/arch_update/executable_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'arch_update/executable'
+
+RSpec.describe ArchUpdate::Executable do
+  class Klass
+    include ArchUpdate::Executable
+  end
+
+  let(:instance) { Klass.new }
+
+  describe '#executable?(path)' do
+    subject { instance }
+    context 'With absolute path' do
+      it "It's valid" do
+        is_expected.to be_executable '/usr/sbin/ls'
+      end
+      it "It's invalid" do
+        is_expected.to_not be_executable '/usr/sbin/hoge_invalid'
+      end
+    end
+    context 'Without path' do
+      it "It's valid" do
+        is_expected.to be_executable 'ls'
+      end
+      it "It's invalid" do
+        is_expected.to_not be_executable 'hoge_invalid'
+      end
+    end
+  end
+end
